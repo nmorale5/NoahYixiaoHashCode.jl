3436bfd5552a2568f5321b543e9dadc8b27bf9ac
Yixiao-Zhang <yixiaoz@mit.edu>
2022-12-03 16:41:49 -0500

    Add a lookahead to the original algorithm

--- src/solver_greedy.jl
+++ src/solver_greedy.jl
@@ -1,8 +1,22 @@
-function heuristic_greedy(j₀, j₁, visited::Vector{Int}, problem::RoutingProblem)
+@inline function heuristic_greedy(j₀, j₁, nvisited::Vector{<:Integer}, problem::RoutingProblem, ::Val{search_depth}) where search_depth
     sid = street_id(j₀, j₁, problem)
     street = problem.streets[sid]
-    eff = distance(street) / time_cost(street)
-    return (-visited[sid], eff)
+    if search_depth == 0
+        a, b, c = (0, 0, 0)
+    else
+        v₀ = nvisited[sid]
+        nvisited[sid] += 1
+        h_max = (-problem.n_streets, 0, 0)
+        for j in outneighbors(j₁, problem)
+            h₁ = heuristic_greedy(j₁, j, nvisited, problem, Val(search_depth-1))
+            if h₁ > h_max
+                h_max = h₁
+            end
+        end
+        a, b, c = h_max
+        nvisited[sid] = v₀
+    end
+    return (a-nvisited[sid], b+distance(street), c-time_cost(street))
 end
 
 """
@@ -15,12 +29,12 @@ function solve_greedy(problem::RoutingProblem)
     t_free_cars = fill(0, problem.n_cars)
     nvisited = fill(0, problem.n_streets)
 
-    for t = 0:problem.total_time
-        for car = 1:problem.n_cars
+    for car = 1:problem.n_cars
+        for t in 0:problem.total_time
             if t >= t_free_cars[car]
                 junc_begin = route(car, solution)[end]
                 junc_end = argmax(
-                    j -> heuristic_greedy(junc_begin, j, nvisited, problem),
+                    j -> heuristic_greedy(junc_begin, j, nvisited, problem, Val(9)),
                     outneighbors(junc_begin, problem),
                 )
 
@@ -34,5 +48,6 @@ function solve_greedy(problem::RoutingProblem)
             end
         end
     end
+    println(total_distance(solution))
     return solution
 end
